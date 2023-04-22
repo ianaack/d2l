09a52310f5a738a53171c07ab2489ffc32c07868
ianaack <ianaack@gmail.com>
2023-04-22 00:04:20 -0400

    Feature: quiz question components

---
+++ src/pages/components/MultipleChoiceQuestions.js
@@ -0,0 +1,29 @@
+export const MultipleChoiceQuestions = [
+	{
+		questionText: "",
+		answerOptions: [
+			{ answerText: "", isCorrect: true },
+			{ answerText: "", isCorrect: true },
+			{ answerText: "", isCorrect: true },
+			{ answerText: "", isCorrect: true },
+		],
+	},
+	{
+		questionText: "",
+		answerOptions: [
+			{ answerText: "", isCorrect: true },
+			{ answerText: "", isCorrect: true },
+			{ answerText: "", isCorrect: true },
+			{ answerText: "", isCorrect: true },
+		],
+	},
+	{
+		questionText: "",
+		answerOptions: [
+			{ answerText: "", isCorrect: true },
+			{ answerText: "", isCorrect: true },
+			{ answerText: "", isCorrect: true },
+			{ answerText: "", isCorrect: true },
+		],
+	},
+];
